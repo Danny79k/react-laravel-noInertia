1ffabb8c4bba2926e53bb375a3b176538181f062
Danny79k <belloli.danny@gmail.com>
2025-02-01 18:24:16 +0000

    pasar a otro ordenador

---
+++ resources/js/react/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navbar } from './components/Navbar'
+//import '../../css/app.css'
+
+function App() {
+  return (
+    <div>
+      <p className="bg-red-500"></p>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ resources/js/react/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div className='bg-red-500'>
+        dsds
+    </div>
+  )
+}
+
