1f553266487a9559874dbcb5a0e0b913e601d011
Danny79k <belloli.danny@gmail.com>
2025-02-03 16:41:30 +0000

    pasando a la A33

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name = $this->faker->words(3, true);
+        return [
+            'name' => $name,
+            'slug' => Str::slug($name),
+            'description' => $this->faker->sentence(),
+            'image' => 'https://picsum.photos/seed/picsum/200/300'
+        ];
+    }
+}
